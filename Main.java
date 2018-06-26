61b1d461675871c99dbcbe99945ee8fa139e6e75
marcaractac <marc.hann@mun.ca>
2018-06-25 22:43:09 -0230

    updated

--- Main.java
+++ Main.java
@@ -3,8 +3,17 @@ package assign3;
 public class Main {
 
 public static void main(String[] args) {
-    //Table A = new Table("A"); 
+ 
     Thread A = new Thread(new Table("A"));
+    Thread B = new Thread(new Table("B"));
+    Thread C = new Thread(new Table("C"));
+    Thread D = new Thread(new Table("D"));
+    Thread E = new Thread(new Table("E"));
+
     A.start();
+    //B.start();
+    //C.start();
+    //D.start();
+    //E.start();
   }
 }
